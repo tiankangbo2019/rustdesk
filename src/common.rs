8f316d3def2f7317efe8638d5f6316cabda5b64c
tiankangbo@outlook.com <tiankangbo@outlook.com>
2026-06-12 23:11:12 +0800

    api

--- src/common.rs
+++ src/common.rs
@@ -1081,7 +1081,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://47.100.164.197:21114".to_owned()
 }
 
 #[inline]
