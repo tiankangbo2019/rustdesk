0ef1dddbb1c72dfc89373db045465ab8e41e93fc
tiankangbo@outlook.com <tiankangbo@outlook.com>
2026-06-13 11:56:11 +0800

    update check is disable

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -3974,20 +3974,7 @@ void earlyAssert() {
 }
 
 void checkUpdate() {
-  if (!isWeb) {
-    if (!bind.isCustomClient()) {
-      platformFFI.registerEventHandler(
-          kCheckSoftwareUpdateFinish, kCheckSoftwareUpdateFinish,
-          (Map<String, dynamic> evt) async {
-        if (evt['url'] is String) {
-          stateGlobal.updateUrl.value = evt['url'];
-        }
-      });
-      Timer(const Duration(seconds: 1), () async {
-        bind.mainGetSoftwareUpdateUrl();
-      });
-    }
-  }
+  // Update check is disabled
 }
 
 // https://github.com/flutter/flutter/issues/153560#issuecomment-2497160535
